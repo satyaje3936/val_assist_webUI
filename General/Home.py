169a5a8a07ac4471176c904226ab270aaa966745
Gupta, Satyajeet <satyajeet.gupta@intel.com>
2025-07-15 16:48:16 +0530

    added PM debug and enhancement script

---
+++ General/Home.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def app():
+    st.title("Generative AI enhanced Validation Assistant for PM")
+    st.write("Welcome to the GenAI Enhanced Validation Assistant page!")
---
+++ General/Settings.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def app():
+    st.title("Settings")
+    st.write("Configure your GPT version and other settings here.")
